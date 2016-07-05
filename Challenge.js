9d53b32cd02ed96e5e34df5c3f77e17aa96ddd0e
Jenna Michel <jemichel@DHF-MBA-11.local>
2016-07-05 14:57:01 -0400

    test

---
+++ Challenge.js
@@ -0,0 +1,13 @@
+
+var myDelay = setTimeout(function(), delay);
+
+
+var myDelay = setTimeout(function(), delay);
+
+            
+ function theEvent() {
+     console.log('Your code that will execute after 3 seconds...');
+ }
+
+var myDelay = setTimeout(function(){
+    console.log('Your code that will execute after 3 seconds...');}, 3000);
---
+++ Functions.js
@@ -0,0 +1,37 @@
+function myFirstFunction() {
+console.log("hello there! I'm a function!);
+}
+var talk = function(saySomething) {
+    console.log(saySomething);
+}
+talk('oh hi!');
+var playerName = function(whoName) {
+    console.log('Your name is: ' = whoName + ' ! ');
+
+}
+function mySecondFunction() {
+    console.log("wassup");
+};
+    
+        
+var myButton = document.getElementById('myButton');
+                   
+                     
+myButton.onclick = function (){
+    console.log('Soccer!');
+};
+var myButton = document.GetElementById{'myButton'}
+    
+function alertbutton()
+{
+alert('soccer is cool');
+}
+myButton.addEventListner("click",alertbutton,false);
+1
+2document.body.style.backgroundColor = "red";
+3
+4
+5
+var myButton = document.getElementById('myButton'); // store the element in a variable
+ 
+0';
